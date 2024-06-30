9f9fbccc69ec8c9d058266e800abd68a6b1ad8cf
77 <2608935022@qq.com>
2024-06-30 14:37:01 +0800

    update Poem Generate

--- app/src/main/java/com/example/owl/ui/courses/SearchCourses.kt
+++ app/src/main/java/com/example/owl/ui/courses/SearchCourses.kt
@@ -18,6 +18,7 @@ package com.example.owl.ui.courses
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -28,6 +29,8 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -77,6 +80,27 @@ import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 
+import androidx.compose.material.Surface
+
+import com.example.owl.ui.theme.OwlTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.ui.unit.dp
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+import android.content.ClipData
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+
+
 suspend fun performLongRunningTask(prompt: String): String {
     val client = OkHttpClient()
     var res = "111"
@@ -107,6 +131,12 @@ suspend fun performLongRunningTask(prompt: String): String {
     return res
 }
 
+fun copyToClipboard(context: Context, text: String) {
+    val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clipData = ClipData.newPlainText("text", text)
+    clipboardManager.setPrimaryClip(clipData)
+}
+
 @Composable
 fun SearchCourses(
     modifier: Modifier = Modifier,
@@ -114,6 +144,8 @@ fun SearchCourses(
 ) {
     var temp = ""
     val (searchTerm, updateSearchTerm) = remember { mutableStateOf(TextFieldValue("")) }
+    val context = LocalContext.current
+    val showSecondBox = remember { mutableStateOf(false) }
     LazyColumn(
         modifier = modifier
             .statusBarsPadding()
@@ -125,7 +157,7 @@ fun SearchCourses(
         item {
             IconButton(onClick = {
                 navController.navigate("courses/my")
-            } ) {
+            }) {
                 Icon(
                     imageVector = Icons.Rounded.ArrowBack,
                     contentDescription = stringResource(R.string.label_back)
@@ -157,7 +189,7 @@ fun SearchCourses(
                     keyboardOptions = KeyboardOptions.Default.copy(
                         // 定义按下回车时的行为
                         imeAction = ImeAction.Done
-                                ),
+                    ),
 
                     keyboardActions = KeyboardActions(
                         onDone = {
@@ -170,6 +202,7 @@ fun SearchCourses(
 
                             textState.value = TextFieldValue()
 
+                            showSecondBox.value = true
 
                         }
                     )
@@ -177,18 +210,102 @@ fun SearchCourses(
 
 
             }
-            Box( modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-                contentAlignment = Alignment.Center){
-
-                Text(
-                    text = temp,
+            if (showSecondBox.value) {
+                Box(
                     modifier = Modifier
-                        // 添加顶部间距
-                        .align(Alignment.Center), // 文本居底部居中对齐
-                )
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Surface(
+                        modifier = Modifier.padding(16.dp), // 为Surface添加4dp的外边距
+//                    elevation =  300.dp, // 设置Surface的阴影高度
+                        // 创建一个自定义的RoundedCornerShape，并设置不同的圆角大小
+                        shape = RoundedCornerShape(
+                            topStart = 16.dp,    // 左上角圆角大小
+                            topEnd = 16.dp,      // 右上角圆角大小
+                            bottomEnd = 16.dp,   // 右下角圆角大小
+                            bottomStart = 16.dp  // 左下角圆角大小
+                        ),
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .align(Alignment.Center) // 确保Column在Surface内居中对齐
+                                .padding(16.dp) // 为内部组件添加间距
+                        ) {
+                            // 您的文本内容
+                            Text(
+//                      text = temp,
+                                text = buildAnnotatedString {
+                                    // 使用 AnnotatedString.Builder 来构建文本并为特定部分设置样式
+                                    withStyle(
+                                        style = SpanStyle(
+                                            fontSize = 24.sp,
+                                            fontWeight = FontWeight.Bold
+                                        )
+                                    ) {
+                                        append("春日\n") // 为第一行设置加大加粗的样式
+                                    }
+                                    // 其他文本使用默认样式
+                                    append(
+                                        "春日融融万物苏，柳丝轻拂绿波湖。\n" +
+                                                "桃花含笑迎风舞，燕子呢喃绕屋庐。\n" +
+                                                "远山如黛添新翠，近水含烟染碧芜。\n" +
+                                                "美景良辰人共醉，诗情画意满皇都。"
+                                    )
+                                },
+                                // 确保文本也居中对齐
+                                modifier = Modifier
+//                                .align(Alignment.Center)
+                                    .fillMaxWidth() // 填充最大宽度
+                                    .padding(16.dp) // 添加内边距
+                                    .heightIn(min = 56.dp), // 设置最小高度，确保文本框有一定的高度
+                                // 样式、颜色等其他属性
+                                textAlign = TextAlign.Center,
+                                color = Color.Black // 例如，设置文本颜色为白色
+                            )
+                            // 添加复制图标
+                            Row(
+                                modifier = Modifier
+                                    .padding(end = 16.dp) // 在按钮右侧添加间距，避免与屏幕边缘重叠
+                            ) {
+                                // 复制按钮
+                                IconButton(onClick = {
+                                    val textToCopy = temp // 假定temp是您要复制的文本
+                                    copyToClipboard(context, textToCopy)
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.ContentCopy,
+                                        contentDescription = "Copy",
+                                        tint = MaterialTheme.colors.onSurface
+                                    )
+                                }
 
+                                // 收藏按钮
+                                IconButton(onClick = {
+                                    // 点击收藏按钮的逻辑
+                                    // 例如，将内容添加到收藏列表
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Bookmark,
+                                        contentDescription = "Favorite",
+                                        tint = MaterialTheme.colors.onSurface
+                                    )
+                                }
+                                IconButton(onClick = {
+                                    // 点击分享按钮的逻辑
+                                    // 例如，使用Intent启动分享对话框
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Share,
+                                        contentDescription = "Share",
+                                        tint = MaterialTheme.colors.onSurface
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
